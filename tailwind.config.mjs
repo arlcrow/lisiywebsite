b570dc4ab60586e76ae187f9fcf6c3296609f3f2
Matvey Aleksandrov <matvey@arlcrow.site>
2024-11-28 16:54:00 +0300

    improve font stack

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			fontFamily: {
+				sans: ['IBM Plex Sans', ...defaultTheme.fontFamily.sans],
+			},
 			colors: {
 				'lisiy-accent': {
 					'50': '#fff9ec',
